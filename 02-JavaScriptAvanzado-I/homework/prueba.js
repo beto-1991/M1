c0ae545fc85402ab8b489eca73ebc2cddb7ca7c4
unknown <juan.garona20@gmail.com>
2023-01-17 15:39:43 -0300

    finished homework

--- 02-JavaScriptAvanzado-I/homework/prueba.js
+++ 02-JavaScriptAvanzado-I/homework/prueba.js
@@ -18,3 +18,123 @@ c(8, 9, 10); // 1.primero se ejecuta esto
 console.log(b); // 6. la consola muestra 10 porque es el valor de b en el contexto global
 console.log(x); // 7. la consola muestra 1 y undefined, porque es una expression en este contexto, pero no esta declarado
 
+/////////////////////////////
+
+console.log(bar); // 1. muestra undefined porque sabe que es una variable pero todavia no esta definida
+console.log(baz); // 2. error porque para mostrar la expression tiene que llamarse despues
+foo(); // 3. no se ejecuta por error en el codigo
+function foo() {
+   console.log('Hola!');
+}
+var bar = 1;
+baz = 2;
+
+///////////////////////////////////
+
+var instructor = 'Tony';
+console.log(instructor);
+if (true) {
+   var instructor = 'Franco';
+}
+console.log(instructor); ///????????????????????????????
+
+/////////////////////////////////////
+
+var instructor = 'Tony';
+let pm = 'Franco';
+if (true) {
+   var instructor = 'The Flash';
+   let pm = 'Reverse Flash';
+   console.log(instructor);
+   console.log(pm);
+}
+console.log(instructor);
+console.log(pm);
+
+/////////////////////////////////////
+
+6 / "3" // 2
+"2" * "3" // 6
+4 + 5 + "px" // 9px
+"$" + 4 + 5 // $45
+"4" - 2 // 2
+"4px" - 2 // NaN
+7 / 0 // Inifinity
+{}[0] // [0]
+parseInt("09") // 9
+5 && 2 // 2
+2 && 5 // 5
+5 || 0 // 5
+0 || 5 // 5
+[3]+[3]-[10]
+3>2>1
+[] == ![]
+
+////////////////////
+
+
+function test() {
+   console.log(a); // 1. Busca la variable a en el contexto. Encuentra que hay una pero todavia no esta definida. por hoisting
+                   //imprime undefined              
+   console.log(foo()); // 2. imprime 2 en la consola segun devuelve la funcion foo()
+
+   var a = 1;
+   function foo() {
+      return 2;
+   }
+}
+
+test();
+
+/////////////////////////
+
+var snack = 'Meow Mix';
+
+function getFood(food) {
+   if (food) {
+      var snack = 'Friskies';
+      return snack;
+   }
+   return snack;
+}
+
+getFood(false);
+
+/////////////////////////
+
+var fullname = 'Juan Perez';
+var obj = {
+   fullname: 'Natalia Nerea',
+   prop: {
+      fullname: 'Aurelio De Rosa',
+      getFullname: function () {
+         return this.fullname;
+      },
+   },
+};
+
+console.log(obj.prop.getFullname());
+
+var test = obj.prop.getFullname;
+
+console.log(test());
+
+
+/////////////////////////////
+
+function printing() {
+   console.log(1);
+   setTimeout(function () {
+      console.log(2);
+   }, 1000);
+   setTimeout(function () {
+      console.log(3);
+   }, 0);
+   console.log(4);
+}
+
+printing();
+
+
+////////////////////////////
+
